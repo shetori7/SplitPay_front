2c6a6ab0dc08ecebf2b7f3bcf7cce179f4bd31ce
shetori <shetori4818@gmail.com>
2025-05-24 16:57:40 +0900

    レイアウトの修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,14 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex flex-col bg-gray-50 text-gray-700">
-      <main className="flex flex-col gap-6 items-center justify-center flex-1 p-8 pb-20 font-[family-name:var(--font-geist-sans)]">
-        <div className="flex flex-col items-center gap-4">
-          <Link
-            href="/group/new"
-            className="bg-[#3f82f5] hover:bg-[#A0C4FF] text-gray-50 rounded px-8 py-3 text-lg font-semibold transition"
-          >
-            はじめる
-          </Link>
-        </div>
-      </main>
+    <div className="flex flex-col items-center justify-center h-full text-gray-700">
+      <h1 className="text-4xl font-bold mb-4">グループを作成</h1>
+      <Link
+        href="/group/new"
+        className="bg-[#3f82f5] text-gray-50 rounded px-8 py-3 text-lg font-semibold"
+      >
+        はじめる
+      </Link>
     </div>
   );
 }
